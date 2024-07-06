06ffb9de0451211d4f494a06c71f1e2b41d5179a
62wip <147309505+62wip@users.noreply.github.com>
2024-07-07 01:00:22 +0300

    Create config-EXAPLE.py

---
+++ config-EXAPLE.py
@@ -0,0 +1,7 @@
+from datetime import timezone, timedelta
+
+API_TOKEN = 'YOUR_API_TOKEN'
+PROXY = 'http://proxy.server:3128'
+TZ = timezone(timedelta(hours=3))
+TG_ID = 'YOUR_TG_ID'
+JSON_FILE_NAME = 'chat_data.json'
